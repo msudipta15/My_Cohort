0a60f516bd83f06d292499e9a012b16e5ca70405
Turbobot <turbobot@vercel.com>
2025-04-23 10:56:15 +0530

    feat(create-turbo): apply official-starter transform

--- week-21-MonoRepo/chatapp/apps/web/app/page.tsx
+++ week-21-MonoRepo/chatapp/apps/web/app/page.tsx
@@ -94,7 +94,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go turbo.build →
+          Go to turbo.build →
         </a>
       </footer>
     </div>
